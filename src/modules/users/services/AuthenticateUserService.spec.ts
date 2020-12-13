c12577b504b5e8f3ae9b958ec5d4dd1ab903a8eb
Paulohfreire <paulo.freire53@gmail.com>
2020-12-13 11:24:03 -0300

    feat:update avatar service tests

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,46 +2,36 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
-
+import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
-describe('CreateUser', () => {
-  it('should be able to create a new user', async () => {
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
     const createUser = new CreateUserService(
       fakeUsersRepository,
       fakeHashProvider,
     );
+
     const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123123',
     });
 
-    expect(user).toHaveProperty('id');
-  });
-
-  it('should not be able to create a new user with email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-    await createUser.execute({
-      name: 'John Doe',
+    const response = await authenticateUser.execute({
       email: 'johndoe@example.com',
       password: '123123',
     });
 
-    expect(
-      createUser.execute({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123123',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,8 @@
 //validando se o usuário de fato existe e excluindo uma imagem antiga caso possua, mantendo apenas a nova.
 
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
-import uploadConfig from '@config/upload';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 
@@ -21,6 +19,8 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -33,20 +33,14 @@ class UpdateUserAvatarService {
     // se existir, deletar avatar anterior
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
+      const fileName = await this.storageProvider.saveFile(avatarFilename);
 
-    // Atualiza o avatar e salva
-    user.avatar = avatarFilename;
+      user.avatar = fileName;
+      await this.usersRepository.save(user);
 
-    await this.usersRepository.save(user);
-
-    return user;
+      return user;
   }
 }
 
