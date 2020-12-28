7ac589089e3c4bda0b52562bc9199834c93614aa
Paulohfreire <paulo.freire53@gmail.com>
2020-12-28 18:48:42 -0300

    feat: create cache provider

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'paulo.freire53@gmail.com',
+      name: 'Paulo',
+    },
+  },
+} as IMailConfig;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,26 +1,4 @@
-import { container } from 'tsyringe';
-
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import IMailprovider from './MailProvider/models/IMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailprovider>(
-  'MailProvider', 
-  container.resolve(EtherealMailProvider),
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
+import './CacheProvider';
